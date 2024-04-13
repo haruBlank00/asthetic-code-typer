ebc96bd2bcdb3e907094ae557fdbf81278ef15a7
haruBlank <harublank01@gmail.com>
2024-04-13 22:09:34 +0545

    scrap a list of supported language by prsim

---
+++ web-scrap/main.py
@@ -0,0 +1,48 @@
+"""
+A scraper to get the list of languages supported by prismjs
+Site: https://lucidar.me/en/web-dev/list-of-supported-languages-by-prism/
+
+DOM Tree:
+    table
+        tbody
+            tr
+                td: language
+                td: key
+                td: class
+"""
+import requests
+from bs4 import BeautifulSoup
+import pyperclip
+import json
+
+url = 'https://lucidar.me/en/web-dev/list-of-supported-languages-by-prism/'
+response = requests.get(url)
+
+is_success = response.status_code == 200
+if not is_success:
+    print("Request failed.")
+else:
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    table = soup.find("tbody")
+    table_not_found = table == None
+    if table_not_found:
+        print("Table element not found. Where did it go?")
+    else:
+        rows = table.find_all("tr") # type: ignore
+        
+        supported_languages = []
+        for row in rows:
+            cells = row.find_all("td")
+
+            row_data = [cell.get_text(strip=True) for cell in cells]
+            language, key, className = row_data
+
+            supported_languages.append({
+                'language': language,
+                'key': key,
+                'className': className
+            })
+        print(supported_languages)
+        pyperclip.copy(json.dumps(supported_languages))
+        
