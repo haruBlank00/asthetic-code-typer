d69edcb55c64c058e81fd8d1de19201ddfa8e1f3
haruBlank <harublank01@gmail.com>
2024-04-15 10:34:41 +0545

    Add switch and text area to form builder

--- src/components/FormBuilder/index.tsx
+++ src/components/FormBuilder/index.tsx
@@ -15,19 +15,22 @@ import {
   FieldValues,
   Path,
 } from "react-hook-form";
+import { Switch } from "../ui/switch";
+import { Textarea } from "../ui/textarea";
 
 type TRender<T extends FieldValues> = {
   controllerField: ControllerRenderProps<T, Path<T>>;
-  field: InputField;
+  field: InputField<T>;
 };
-export interface InputField extends InputProps {
+export interface InputField<T extends FieldValues> extends InputProps {
   label: string;
   description?: string;
-  render?: ({ field }: TRender) => ReactElement;
+  render?: ({ field }: TRender<T>) => ReactElement;
+  el?: "switch" | "textArea";
 }
 
 type FormBuilderProps<T extends FieldValues> = {
-  fields: InputField[];
+  fields: InputField<T>[];
   control: Control<T>;
 };
 
@@ -41,11 +44,10 @@ export function FormBuilder<T extends FieldValues>({
   }
 
   return fields.map((field) => {
-    const { name, label, description, placeholder, render } = field;
+    const { name, label, description, placeholder, render, el } = field;
 
     const hasDescripton = Boolean(description);
     const hasCustomRender = render !== undefined;
-
     if (hasCustomRender) {
       return (
         <FormField
@@ -60,6 +62,60 @@ export function FormBuilder<T extends FieldValues>({
           }
         />
       );
+    }
+
+    const isOtherThanInputElement = Boolean(el);
+    if (isOtherThanInputElement) {
+      if (el === "switch") {
+        return (
+          <FormField
+            key={name}
+            control={control}
+            name={name! as Path<T>}
+            render={({ field }) => (
+              <FormItem className="flex gap-4 items-center">
+                <FormControl>
+                  <Switch
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <FormLabel className="text-lg">{label}</FormLabel>
+
+                <If condition={hasDescripton}>
+                  <FormDescription>{description}</FormDescription>
+                </If>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        );
+      }
+
+      if (el === "textArea") {
+        return (
+          <FormField
+            key={name}
+            control={control}
+            name={name! as Path<T>}
+            render={({ field }) => (
+              <FormItem className="flex flex-col gap-4 items-center">
+                <FormLabel className="text-lg">{label}</FormLabel>
+                <FormControl>
+                  <Textarea placeholder={placeholder} {...field} rows={10} />
+                </FormControl>
+
+                <If condition={hasDescripton}>
+                  <FormDescription>{description}</FormDescription>
+                </If>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        );
+      }
     }
     return (
       <FormField
